bc95157eb762c55d0e38af33c2a23645273ff889
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-03-15 17:00:11 +0300

    Add sign-in route and create sign-in view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('sign_up');
 });
+Route::get('/sign-in', function () {
+    return view('sign_in');
+});
+Route::get('/sign-up', function () {
+    return view('sign_up');
+});
